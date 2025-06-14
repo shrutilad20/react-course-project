6fc1bbf9901914cfc09e0e4672a221c69934ba18
shrutilad20 <shrutilad35@gmail.com>
2025-06-14 21:13:52 +0530

    files

--- paradise-nursery/src/redux/store.js
+++ paradise-nursery/src/redux/store.js
@@ -1,6 +1,6 @@
 // src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice';
+import cartReducer from './cartslice';
 
 export const store = configureStore({
   reducer: {
